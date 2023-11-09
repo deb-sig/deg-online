d35a34f3af4f13afb7055875faf35d17b7029e41
TripleZ <me@triplez.cn>
2023-11-10 02:01:39 +0800

    fix(Translate): cannot choose provider
    Signed-off-by: TripleZ <me@triplez.cn>

--- src/components/Translate.vue
+++ src/components/Translate.vue
@@ -27,7 +27,7 @@ const translateBill = async () => {
   <!-- <h1>{{ msg }}</h1> -->
   <div class="mx-auto mt-8 w-4/5">
     <div class="flex my-3">
-      <Listbox v-model="providerCode" class="w-60" />
+      <Listbox v-model:provider="providerCode" class="w-60" />
       <button
         type="button"
         class="ml-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
